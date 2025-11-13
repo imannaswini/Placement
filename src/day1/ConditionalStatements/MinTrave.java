65c0c5b0ea2d59c3af92ebcc9335c6d8f1b41f80
imannaswini <iammannaswini@gmail.com>
2025-11-13 15:15:56 +0530

    modifed

--- src/day1/ConditionalStatements/MinTrave.java
+++ src/day1/ConditionalStatements/MinTrave.java
@@ -5,7 +5,23 @@ public class MinTrave
 {
     public static void main(String[] ags)
     {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int v1=sc.nextInt();
+        int v2=sc.nextInt();
+        double st=(Math.sqrt(2)*n)/v1;
+        double et =(2*n)/v2;
+        if(et<st)
+        {
+            System.out.println("Elevator");
+        }
+        else
+        {
+            System.out.println("Stairs");
+        }
+
 
     }
     
+
 }
