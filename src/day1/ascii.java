ee3efc7b13ec438944d3e5895d90cb5ab994c93e
imannaswini <iammannaswini@gmail.com>
2025-11-13 11:17:28 +0530

    day1 ascii file modified

--- src/day1/ascii.java
+++ src/day1/ascii.java
@@ -3,15 +3,30 @@ import java.util.*;
 public class ascii 
 {    public static void main(String[] args) 
     {
-       int n;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter a digit:");
-        for(int i=1;i<=5;i++)
-        {
-            n = sc.nextInt();
-            char c = (char) n;
-            System.out.println( n + "- " + c);
-        }
+    //    int n;
+    //     Scanner sc=new Scanner(System.in);
+    //     System.out.println("Enter a digit:");
+    //     for(int i=1;i<=5;i++)
+    //     {
+    //         n = sc.nextInt();
+    //         char c = (char) n;
+    //         System.out.println( n + "- " + c);
+    //     }
+     Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sample Input 1:");
+        System.out.println("Enter the digits:");
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+
+        System.out.println("\nSample Output 1:");
+        System.out.println(n1 + "-" + (char)n1);
+        System.out.println(n2 + "-" + (char)n2);
+        System.out.println(n3 + "-" + (char)n3);
+        System.out.println(n4 + "-" + (char)n4);
         
 }
 }
